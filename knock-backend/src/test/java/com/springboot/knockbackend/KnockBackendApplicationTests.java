f4853ad667e8cea9deb7f5c79284c62046e708f2
Hwanghyeong_ <80672495+HwangHyeongjin@users.noreply.github.com>
2025-07-29 12:57:13 +0900

    Feat/setting
    백엔드 환경세팅

---
+++ knock-backend/src/test/java/com/springboot/knockbackend/KnockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.knockbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnockBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
